7797909742122264290958a8f8e33414179a02f8
hellowhare <142896328+hellowhare@users.noreply.github.com>
2023-08-22 14:12:36 +0300

    Create main.py
    finished project

---
+++ main.py
@@ -0,0 +1,21 @@
+text = input('Введите текст: ')
+text = text.lower()
+punctuation = ['.', ',', '!', '?', '-', ':', ';']
+for i in punctuation:
+    text = text.replace(i, '')
+words = text.split()
+long = words[0]
+for i in words:
+    if len(i) > len(long):
+        long = i
+short = words[0]
+for i in words:
+    if len(i) < len(short):
+        short = i
+num = {}
+for i in words:
+    num[i] = words.count(i)
+print('Количество разных слов: ', len(set(words)))
+print('Самое длинное слово: ', long)
+print('Самое короткое слово: ', short)
+print('Частота символов: ', num)
